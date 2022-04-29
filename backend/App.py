a759afd7ed9db7619431a5b8437a7527e0072fe1
nicobartok0 <nicobartok0@gmail.com>
2022-04-29 09:38:56 -0300

    29/04 Conexión a base de datos

--- backend/App.py
+++ backend/App.py
@@ -1,8 +1,24 @@
 from flask import Flask
+from flask_mysqldb import MySQL
 
+#Se define la aplicación como un objeto de la clase Flask
 app = Flask(__name__)
+#Configuración de servidor MySQL (CAMBIAR AL GENERAR LA APLICACIÓN FINAL)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'mbbs_db'
+mysql = MySQL(app)
+
+#Ruta principal
+@app.route('/')
+def Index():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM usuario')
+    data = cur.fetchall()
+    return str(data)
 
 
-
+#Bucle principal. La aplicación se corre en el puerto 3000.
 if (__name__) == '__main__':
     app.run(port=3000, debug=True)
