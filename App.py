2bde7fd9eb4a59d85e907fbbe5dae15ce6229d6d
nicobartok0 <nicobartok0@gmail.com>
2022-05-03 09:33:08 -0300

    Registro de usuario funcional. MBBS v.0.1

--- App.py
+++ App.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from click import password_option
+from flask import Flask, render_template, request, url_for
 from flask_mysqldb import MySQL
 
 #Se define la aplicación como un objeto de la clase Flask
@@ -14,6 +15,21 @@ mysql = MySQL(app)
 @app.route('/')
 def Index():
     return render_template('index.html')
+
+#Ruta de registro de contacto
+@app.route('/add_contact', methods=['POST'])
+def add_contact():
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        password = request.form['password']
+        email = request.form['email']
+        cur = mysql.connection.cursor()
+        cur.execute('INSERT INTO usuario (nombre_usuario, email, contraseña) VALUES (%s, %s, %s)', 
+        (fullname, email, password))
+        mysql.connection.commit()
+        return render_template('index.html')
+
+
     
 
 
